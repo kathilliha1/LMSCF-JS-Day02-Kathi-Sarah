8a411d001aabf19ac0b61355cf0f2d3b11755942
sarahvic <sarahley93@gmail.com>
2020-01-27 15:34:00 +0100

    background colors

--- Sarah/advanced/script.js
+++ Sarah/advanced/script.js
@@ -23,3 +23,15 @@ function yesterdayDate(){
 	document.getElementById("result").innerHTML = "Yesterday's date: " + date.toDateString();
 }
 
+// exercise 3
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+ console.log(bgColor);
+  
+    document.body.style.background = bgColor;
+    }
+
+random_bg_color();
